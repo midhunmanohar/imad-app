be45ca480973c252060efee388d8978d42800e3b
midhun manohar <midhmanohar@gmail.com>
2018-02-24 12:20:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,17 @@
 console.log('Loaded!');
 
-document.getElementById("main-text").innerHTML="Hello";
+// document.getElementById("main-text").innerHTML="Hello";
 
-var img=document.getElementById("madi");
-img.onclick=function(){
-    img.style.marginLeft='100px';
+// var img=document.getElementById("madi");
+// img.onclick=function(){
+//     img.style.marginLeft='100px';
+// }
+
+var button=document.getElementById("counter");
+var counter=0;
+var span=document.getElementById("count");
+button.onclick=function(){
+    counter+=1;
+    span.innerHTML=counter.toString();
 }
+
