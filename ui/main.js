04ebf60b479af4b90d49112533c62de1a34e20df
midhun manohar <midhmanohar@gmail.com>
2018-02-26 22:19:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,21 +8,23 @@ console.log('Loaded!');
 // }
 
 var button = document.getElementById('counter');
-//var counter = 0;
+
 button.onclick = function (){
     
     var request = new XMLHttpRequest();
+    
     request.onreadystatechange = function () {
         if (request.readyState === XMLHttpRequest.DONE){
-        if (request.status === 200){
-           var counter = request.responseText;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
-}
-}
-};
+            if (request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+         }
+      }
+  };
 request.open('GET', 'http://midhunmanohar.imad.hasura-app.io/counter', true);
 request.send(null);
+    
 };
 
 // var counter=0;
