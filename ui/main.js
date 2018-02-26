1b6265555e65f4c44f3c8250e3cc003c65be23be
midhun manohar <midhmanohar@gmail.com>
2018-02-26 21:46:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,29 +7,23 @@ console.log('Loaded!');
 //     img.style.marginLeft='100px';
 // }
 
-var button=document.getElementById("counter");
-
-button.onclick=function(){
-    //Create a request
-    var request= new XMLHttpRequest();
-    // capture the response and store it in a variable
-    request.onreadystatechange =function(){
-        if(request.readystate===XMLHttpRequest.DONE){
-            // Take some action
-            if(request.status ===200){
-                var counter=request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML=counter.toString();
-            }
-        }
-    };
+var button = document.getElementById('counter');
+//var counter = 0;
+button.onclick = function (){
     
-    // make request
-    
-    request.open('GET','http://midhmanohar.imad.hasura-app.io/counter',true);
-    request.send(null);
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE){
+        if (request.status === 200){
+           var counter = request.responseText;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+}
+}
 };
-
+request.open('GET', 'http://u034senthil.imad.hasura-app.io/counter', true);
+request.send(null);
+};
 
 // var counter=0;
 // var span=document.getElementById("count");
