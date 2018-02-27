0945d1bd4224f8fc4ee14715c4243005e7caced8
midhun manohar <midhmanohar@gmail.com>
2018-02-27 21:05:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -39,14 +39,28 @@ var nameInput=document.getElementById("name").value;
 var submit=document.getElementById("submit");
 submit.onclick=function(){
     
-    var names=['name1','name2','name3','name4'];
-    var list='';
-    for(var i=0;i<names.length;i++){
-        list+='<li>'+names[i]+'</li>';
-    }
-    var ul=document.getElementById('namelist');
-    ul.innerHTML=list;
-}
+   // create the request
+   var request = new XMLHttpRequest();  
+    
+   request.onreadystatechange = function () { 
+            if (this.readyState == 4 && this.status == 200){
+              
+            var names=request.responseText;
+            names=JSON.parse(names);
+            var list='';
+            for(var i=0;i<names.length;i++){
+                list+='<li>'+names[i]+'</li>';
+            }
+            var ul=document.getElementById('namelist');
+            ul.innerHTML=list;  
+                      
+         }  
+       
+  }; 
+          //make the request
+        request.open("GET", "http://midhmanohar.imad.hasura-app.io/submit-name?name="+nameInput, true);
+        request.send(null);
+};
 
 
 
