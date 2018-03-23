e2eb200b47469679399e9d8e2c42904d1f50a693
midhun manohar <midhmanohar@gmail.com>
2018-03-23 21:35:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -147,7 +147,7 @@ app.post('/login',function(req,res){
        } else{
            if(result.rows.length===0)
            {
-               res.status(403).send("username/password is invalid")
+               res.status(403).send("username/password is invalid");
            }else{
                 var dbString=result.rows[0].password;
                 var salt= dbString.split('$')[2];
@@ -156,7 +156,7 @@ app.post('/login',function(req,res){
                     res.send("Login Successfully");
                 }else{
                     
-                    res.status(403).send("username/password is invalid")
+                    res.status(403).send("username/password is invalid");
                 }
                 
            }
