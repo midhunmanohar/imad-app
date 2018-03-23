331b0cbae87785ce92c99f35510cd68eaa4026ba
midhun manohar <midhmanohar@gmail.com>
2018-03-23 21:24:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -117,6 +117,20 @@ function createTemplate(data){
 app.post('/create-user',function(req,res){
     var username=req.body.username;
     var password=req.body.password;
+    var salt = crypto.randomBytes(128).toString('hex');
+    var dbString = hash(password,salt);
+    pool.query('INSERT INTO "user" (username,password) VALUES ($1,$2)',[username,dbString],function(err,result){
+       if(err){
+           res.status(500).send(err.toString());
+       } else{
+           res.send("User successfully created :"+username);
+       }
+    }); 
+    
+});
+
+app.post('/login',function(req,res){
+    
     
     pool.query('SELECT * FROM "user" WHERE username = $1',[username],function(err,result){
        if(err){
@@ -140,20 +154,6 @@ app.post('/create-user',function(req,res){
       
        }
     });
-});
-
-app.post('/login',function(req,res){
-    var username=req.body.username;
-    var password=req.body.password;
-    var salt = crypto.randomBytes(128).toString('hex');
-    var dbString = hash(password,salt);
-    pool.query('INSERT INTO "user" (username,password) VALUES ($1,$2)',[username,dbString],function(err,result){
-       if(err){
-           res.status(500).send(err.toString());
-       } else{
-           res.send("User successfully created :"+username);
-       }
-    }); 
            
 });
 
