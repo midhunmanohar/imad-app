c9d7e287382e42cabfa408468bad249ff63d99ac
midhun manohar <midhmanohar@gmail.com>
2018-03-24 17:52:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,35 @@
 //     img.style.marginLeft='100px';
 // } 
 
+
+function loadArticles () {
+        // Check if the user is already logged in
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            var articles = document.getElementById('articles');
+            if (request.status === 200) {
+                var content = '<ul>';
+                var articleData = JSON.parse(this.responseText);
+                for (var i=0; i< articleData.length; i++) {
+                    content += `<li>
+                    <a href="/articles/${articleData[i].title}">${articleData[i].heading}</a>
+                    (${articleData[i].date.split('T')[0]})</li>`;
+                }
+                content += "</ul>"
+                articles.innerHTML = content;
+            } else {
+                articles.innerHTML('Oops! Could not load all articles!')
+            }
+        }
+    };
+    
+    request.open('GET', '/get-articles', true);
+    request.send(null);
+}
+
+
+
 var button = document.getElementById('counter'); 
 
 button.onclick = function (){ 
@@ -126,6 +155,8 @@ login.onclick=function(){
              
              document.getElementById("login_area").innerHTML=loadLogoutForm(username);
              
+             loadArticles ();
+             
             }  else if(this.status==403){
                 alert("Username/Password is incorrect");
             }else if(this.status==500){
@@ -180,6 +211,8 @@ register.onclick=function(){
 
     
 };
+
+
 
 
 
