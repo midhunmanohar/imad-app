9337a09702f35142b4ba026d6a8ea1bd540028ab
midhun manohar <midhmanohar@gmail.com>
2018-03-24 16:32:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -229,7 +229,7 @@ app.get('/submit-comment',function(req,res) {
 
 
 
-app.get('/:articleName',function(req,res){
+app.get('/articles/:articleName',function(req,res){
     
    var articleName=req.params.articleName;
    
