a8feef26b6eb04696373a575ad3f6b290c5cc078
midhun manohar <midhmanohar@gmail.com>
2018-03-25 11:25:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -68,10 +68,20 @@ function createTemplate(data){
   return htmlTemplate;  
 }
 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+
 function hash(input,salt){
     var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
     return hashed.toString('hex');
 }
+
+app.get('/hash/:input',function(req,res){
+   var hashedString=hash(req.params.input,"this is some random string");
+   res.send(hashedString);
+});
 
 app.post('/create-user',function(req,res){
     var username=req.body.username;
@@ -141,9 +151,6 @@ app.get('/submit-name',function(req,res){
 });
 
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
 
 app.get('/get-articles',function(req,res){
     
