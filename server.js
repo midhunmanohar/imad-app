bb965cf5d29555366ea6df247bbdb8a12929057e
midhun manohar <midhmanohar@gmail.com>
2018-03-25 11:43:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -155,6 +155,26 @@ app.get('/get-articles',function(req,res){
 });
 
 
+app.get('/articles/:articleName',function(req,res){
+    
+   var articleName=req.params.articleName;
+
+     pool.query('SELECT * FROM article WHERE title = $1 ',[req.params.articleName],function(err,result){
+      if(err){
+          res.status(500).send(err.toString());
+      } else{
+          if(result.rows.length===0){
+              res.status(404).send("article not found");
+          }else{
+            var articleData = result.rows[0];
+            res.send(createTemplate(articleData));
+          }
+      }
+   });
+   
+});
+
+
 var names=[];
 app.get('/submit-name',function(req,res){
    
@@ -175,24 +195,6 @@ app.get('/counter',function(req,res){
     res.send(counter.toString());
 });
 
-app.get('/articles/:articleName',function(req,res){
-    
-   var articleName=req.params.articleName;
-
-     pool.query('SELECT * FROM article WHERE title = $1 ',[req.params.articleName],function(err,result){
-      if(err){
-          res.status(500).send(err.toString());
-      } else{
-          if(result.rows.length===0){
-              res.status(404).send("article not found");
-          }else{
-            var articleData = result.rows[0];
-            res.send(createTemplate(articleData));
-          }
-      }
-   });
-   
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
